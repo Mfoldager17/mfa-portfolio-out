540f1b1e0c831dc03924a6ea4a645f2d3323bf3f
Mfoldager17 <61451322+Mfoldager17@users.noreply.github.com>
2023-09-18 08:58:26 +0000

    testing codespace

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -6,6 +6,7 @@ const AboutSection = () => {
         <h1 className="text-[#b8b6b6]"></h1>
         <div id="about" className="mt-24 rounded-[115px] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] xl:w-[500px] xl:h-[500px]">
         </div>
+        Dette er en test
     </section>
   )
 }
